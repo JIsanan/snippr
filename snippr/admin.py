a77949962ba01636b38a9bc7b17c65d18729f50b
John Matthew Flores <joflo.matres@gmail.com>
2018-10-16 02:51:25 +0800

    Add Activity Model

--- snippr/admin.py
+++ snippr/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 from .models.userprofile import UserProfile
-from .models.commit import Commit
+from .models.commit import Commit, Activity
 
 admin.site.register(UserProfile)
 admin.site.register(Commit)
+admin.site.register(Activity)
--- snippr/serializers/commit.py
+++ snippr/serializers/commit.py
@@ -1,17 +1,28 @@
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
-
 from snippr.models.commit import Commit
 
 
 class CommitSerializer(serializers.ModelSerializer):
-	username = serializers.SerializerMethodField()
+    username = serializers.SerializerMethodField()
+    upvotes = serializers.SerializerMethodField()
 
-	class Meta:
-		model = Commit
-		fields = (
-			'pk', 'user', 'username', 'language', 'code',)
+    class Meta:
+        model = Commit
+        fields = (
+            'pk',
+            'user',
+            'username',
+            'language',
+            'code',
+            'date_created',
+            'title',
+            'upvotes')
 
-	def get_username(self, obj):
-		ret = obj.user.username
-		return ret
+    def get_username(self, obj):
+        ret = obj.user.username
+        return ret
+
+    def get_upvotes(self, obj):
+        ret = obj.upvote.count()
+        return ret
