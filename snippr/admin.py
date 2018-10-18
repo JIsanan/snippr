68507005d746a823f6029938f4ad8ea35035abd8
John Matthew Flores <joflo.matres@gmail.com>
2018-10-18 17:13:22 +0800

    Add Languages to admin page

--- snippr/admin.py
+++ snippr/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 from .models.userprofile import UserProfile
-from .models.commit import Commit, Activity
+from .models.commit import Commit, Activity, Language
 
 admin.site.register(UserProfile)
 admin.site.register(Commit)
 admin.site.register(Activity)
+admin.site.register(Language)
