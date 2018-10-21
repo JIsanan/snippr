bcd28a0bd7cf991373ebae1c63d348ad48a2f8b7
allenazamoras <allenazamoras@gmail.com>
2018-10-21 18:47:27 +0800

    Add filter for issues/commit based on language

--- snippr/settings.py
+++ snippr/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'snippr',
     'rest_framework',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -113,6 +114,9 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
     ),
 }
 
