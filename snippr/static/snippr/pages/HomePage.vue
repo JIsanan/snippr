d07e4adf970bae8ee6fa04448255186a770d748c
John Matthew Flores <joflo.matres@gmail.com>
2018-10-23 06:41:15 +0800

    Add change in text size in landing

--- snippr/static/snippr/pages/HomePage.vue
+++ snippr/static/snippr/pages/HomePage.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="columns is-centered landing">
-    	<div class="column is-3">
+    	<div class="column is-4">
     		<div class="content">
-    			<h1 class="title">Welcome to Snippr!</h1>
-    			<p>Snippr is the all new network for developers to have elaborative and friendly environment which allows them to exchange knowledge related to anything about computer programming. With Snippr there's no need to manually keep track of code versions Snippr has got that all covered! Join us now!</p>
+    			<h1 class="title is-2">Welcome to Snippr!</h1>
+    			<p class="is-size-5">Snippr is the all new network for developers to have elaborative and friendly environment which allows them to exchange knowledge related to anything about computer programming. With Snippr there's no need to manually keep track of code versions Snippr has got that all covered! Join us now!</p>
     		</div>
     	</div>
     	<div class="column is-3">
