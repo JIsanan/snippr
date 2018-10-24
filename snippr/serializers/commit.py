ad8fbf929e00e06c68c8bee94800ff66831f0feb
Zeshiba <joshuaisanan@gmail.com>
2018-10-24 21:11:14 +0800

    Added Changes

--- snippr/serializers/commit.py
+++ snippr/serializers/commit.py
@@ -1,27 +1,45 @@
 from rest_framework import serializers
-from snippr.models.commit import Commit
+from snippr.models.commit import Commit, Snippet, Language
 
+
+class SnippetSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Snippet
+        fields = (
+            'pk', 'code', 'date_created',)
+
+class LanguageSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Language
+        fields = (
+            'pk', 'name')
 
 class CommitSerializer(serializers.ModelSerializer):
     username = serializers.SerializerMethodField()
     user_id = serializers.SerializerMethodField()
     upvotes = serializers.SerializerMethodField()
-    language = serializers.SerializerMethodField()
     has_upvoted = serializers.SerializerMethodField()
+    language_name = serializers.SerializerMethodField()
 
     class Meta:
         model = Commit
         fields = (
             'pk',
+            'user',
             'user_id',
             'username',
+            'language_name',
             'language',
-            'code',
             'date_created',
             'title',
             'description',
             'upvotes',
             'has_upvoted')
+        extra_kwargs = {
+            'user': {'write_only': True}
+        }
 
     def get_username(self, obj):
         ret = obj.user.username
@@ -31,7 +49,7 @@ class CommitSerializer(serializers.ModelSerializer):
         ret = obj.user.userprofile.id
         return ret
 
-    def get_language(self, obj):
+    def get_language_name(self, obj):
         ret = obj.language.name
         return ret
 
